d43115ee1a011cb4756fd4770273215c5eb57757
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-05-24 21:46:13 -0300

    Feat(NavBar): Update component and styles to SCSS

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,65 +1,67 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import logo from '../../assets/img/logo.svg'
-import navIcon1 from '../../assets/img/nav-icon1.svg'
-import navIcon2 from '../../assets/img/nav-icon2.svg'
-import navIcon3 from '../../assets/img/nav-icon3.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.png'
+import { FaBars, FaTimes, FaWhatsapp, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const navBar = () => {
-    const [activeLink, setActiveLink] = useState('home')
-    const [scrolled, setScrolled] = useState(false)
+	const [activeLink, setActiveLink] = useState('home')
+	const [scrolled, setScrolled] = useState(false)
+	const [menuExpanded, setMenuExpanded] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true)
-            } else {
-                setScrolled(false)
-            }
-        }
+	useEffect(() => {
+		const onScroll = () => {
+			if (window.scrollY > 50) {
+				setScrolled(true)
+			} else {
+				setScrolled(false)
+			}
+		}
 
-        window.addEventListener("scroll", onScroll)
+		window.addEventListener("scroll", onScroll)
 
-        return () => window.removeEventListener("scroll", onScroll)
-    }, [])
+		return () => window.removeEventListener("scroll", onScroll)
+	}, [])
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value)
-    }
+	const onUpdateActiveLink = (value) => {
+		setActiveLink(value)
+	}
+	const toggleMenu = () => {
+		setMenuExpanded(!menuExpanded);
+	};
 
-    return (
-        <Navbar expand="lg" className={ scrolled ? "scrolled" : "" }>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={ logo } />
-                </Navbar.Brand>
+	return (
+		<Navbar expand="md" className={ scrolled ? "scrolled" : "" } expanded={ menuExpanded } onToggle={ toggleMenu } >
+			<Container>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className='navbar-toggle-icon'></span>
-                </Navbar.Toggle>
+				<Navbar.Brand href="#home">
+					<img src={ logo } />
+				</Navbar.Brand>
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('home') }>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={ activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('skills') } > Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={ activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('projects') }> Projects</Nav.Link>
-                    </Nav>
-                    <span className='navbar-text'>
-                        <div className='social-icon'>
-                            <a href='#'><img src={ navIcon1 } alt='' /></a>
-                            <a href='#'><img src={ navIcon2 } alt='' /></a>
-                            <a href='#'><img src={ navIcon3 } alt='' /></a>
-                        </div>
-                        <button className='vvd' onClick={ () => console.log('conectou') }><span>Let´s dale</span></button>
-                    </span>
-                </Navbar.Collapse>
-            </Container >
-        </Navbar >
-    );
+				<Navbar.Toggle aria-controls="basic-navbar-nav">
+					{ menuExpanded
+						? <FaTimes className="navbar-toggler-icon" color="white" />
+						: <FaBars className="navbar-toggler-icon" color="white" /> }
+				</Navbar.Toggle>
+
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						<Nav.Link href="#home" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('home') }>Home</Nav.Link>
+						<Nav.Link href="#skills" className={ activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('skills') } > Skills</Nav.Link>
+						<Nav.Link href="#projects" className={ activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={ () => onUpdateActiveLink('projects') }> Projetos</Nav.Link>
+					</Nav>
+
+					<span className='navbar-text'>
+						<div className='social-icon'>
+							<a target="_blank" href='https://www.linkedin.com/in/joão-paulo-l-gonçalves-46a22a244/'><FaLinkedin className='nav-icon' /></a>
+							<a target="_blank" href='https://wa.me/5514996842832'><FaWhatsapp className='nav-icon' /></a>
+							<a target="_blank" href='https://github.com/joaopaulo021/'><FaGithub className='nav-icon' /></a>
+						</div>
+						<a href="#contato"><button className="vvd"><span>Mensagem</span></button></a>
+					</span>
+				</Navbar.Collapse>
+			</Container >
+		</Navbar >
+	);
 }
 
 export default navBar;
