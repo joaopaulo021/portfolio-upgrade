30274701d62ea2e1b598131adbc119bd3c3b9b9a
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-05-24 21:47:00 -0300

    Feat(Projects):Update component and styles to SCSS

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,20 +1,18 @@
 import { Col } from "react-bootstrap"
-import './styles.css'
 
-const ProjectCard = ({ title, description, imgUrl }) => {
-    return (
-        <>
-            <Col size={ 12 } sm={ 6 } md={ 4 }>
-                <div className="proj-imgbx">
-                    <video controls={ true } src={ imgUrl } />
-                    <div className="proj-txtx">
-                        <h4>{ title }</h4>
-                        <span>{ description }</span>
-                    </div>
-                </div>
-            </Col>
-        </>
-    )
+const ProjectCard = ({ title, description, imgUrl, link }) => {
+	return (
+		<Col size={ 12 } sm={ 6 } md={ 4 }>
+			<div className="proj-imgbx">
+				<img src={ imgUrl } />
+				<div className="proj-txtx">
+					<h4>{ title }</h4>
+					<span>{ description }</span>
+					<a target="_blank" href={ link }>Acesse aqui!</a>
+				</div>
+			</div>
+		</Col >
+	)
 }
 
 export default ProjectCard
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,111 +1,135 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap"
 import colorSharp2 from '../../assets/img/color-sharp2.png'
-
 import ProjectCard from "../ProjectCard"
-import './styles.css'
-import projImg1 from "../../assets/videos/projetos-login.mp4"
-import projImg2 from "../../assets/img/project-img2.png"
-import projImg3 from "../../assets/img/project-img3.png"
+import projImg1 from "../../assets/img/webappcontrole.png"
+import projImg2 from "../../assets/img/gravgraf.png"
+import projImg3 from "../../assets/img/nk14.png"
+import projImg4 from "../../assets/img/circuito-mulher.png"
+import projImg5 from "../../assets/img/filmeson.png"
+import projImg6 from "../../assets/img/scrimbo.png"
 
-
+import { useEffect, useRef } from "react"
+import ScrollReveal from "scrollreveal"
 
 const Projects = () => {
 
-    const projects = [{
-        title: "nome do site",
-        description: "descrição do projeto",
-        imgUrl: projImg1,
+  const projectBxRef = useRef(null);
 
-    },
-    {
-        title: "nome do site",
-        description: "descrição do projeto",
-        imgUrl: projImg1,
+  useEffect(() => {
+    const sr = ScrollReveal({ reset: true });
+    sr.reveal(projectBxRef.current, {
+      distance: "50px",
+      origin: "bottom",
+      duration: 1000,
+    });
+  }, []);
 
-    },
-    {
-        title: "nome do site",
-        description: "descrição do projeto",
-        imgUrl: projImg1,
+  const projects = [{
+    title: "Grav Graf Sinalizações",
+    description: "Site desenvolvido para empresa no segmento de placas de sinalização.",
+    link: "https://www.gravgraf.com.br",
+    imgUrl: projImg2,
+  },
 
-    },
-    {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-    },
-    {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-    },
-    {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-    },
-    ]
+  {
+    title: "Circuito Mulher",
+    description: "Pagina desenvolvida p/ o evento online Circuito Mulher com com sistema de autenticação de usuario e chat em tempo real.",
+    link: "https://www.circuitomulher.institutomuitoalemdecinderelas.com/index",
+    imgUrl: projImg4,
+  },
 
+  {
+    title: "NK 14 Sporting",
+    description: "E-commerce completo, desenvolvido para uma loja de camisas de futebol, com diversas funcionalidades e sistema de pagamento online integrado.",
+    link: "https://www.nk14sporting.com.br",
+    imgUrl: projImg3,
+  },
 
-    return (
-        <>
-            <section className="project" id="projects">
-                <Container>
+  {
+
+    title: "Web App finanças",
+    description: "Projeto de finanças que calcula as entradas e saidas e te mostra o total salvando os dados no navegador",
+    link: "https://app-controle-financeiro-dun.vercel.app",
+    imgUrl: projImg1,
+  },
+
+  {
+    title: "Recomendação de filmes",
+    description: "Mostra os lançamentos recentes de filmes com base nas recomendações do themovieDB",
+    link: "https://filmes-on.vercel.app",
+    imgUrl: projImg5,
+  },
+
+  {
+    title: "The Scrimbo Game",
+    description: "um dos primerios projetos que desenvolvi, um minigame estilo 'endless runner' (versão apenas p/ desktop!) ",
+    link: "https://joaopaulo021.github.io/Game-c-Javascript/",
+    imgUrl: projImg6,
+  },
+  ]
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={ 12 }>
+            <div ref={ projectBxRef }>
+              <h2> Projetos</h2>
+              <p>Confira alguns de meus trabalhos!</p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+
+                  <Nav.Item >
+                    <Nav.Link className="nav-item1" eventKey="first">
+                      Pagina 1
+                    </Nav.Link>
+                  </Nav.Item>
+
+                  <Nav.Item >
+                    <Nav.Link eventKey="second">
+                      Pagina 2
+                    </Nav.Link>
+                  </Nav.Item>
+
+                  <Nav.Item >
+                    <Nav.Link className="nav-item3" eventKey="third">
+                      Pagina 3
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+
+                <Tab.Content id="slideInUp">
+                  <Tab.Pane eventKey="first">
                     <Row>
-                        <Col>
-                            <h2>Projetos</h2>
-                            <p>sdaaasd</p>
-                            <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="first">
-                                            Pagina 1
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="second">
-                                            Pagina 2
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="third" disabled>
-                                            Pagina 3
-                                        </Nav.Link>
-                                    </Nav.Item>
-                                </Nav>
-                                <Tab.Content>
-                                    <Tab.Pane eventKey="first">
-                                        <Row>
-                                            {
-                                                projects.map((project, index) => {
-                                                    return (
-                                                        <ProjectCard
-                                                            key={ index }
-                                                            { ...project }
-                                                        />
-                                                    )
-                                                })
-                                            }
-                                        </Row>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="second">
-                                        <p>lorem ipsumlorem ipsumlorem ipsum</p>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="third">
-                                        <p>lorem ipsumlorem ipsumlorem ipsum</p>
-                                    </Tab.Pane>
-                                </Tab.Content>
-                            </Tab.Container>
-                        </Col>
+                      {
+                        projects.map((project, index) => {
+                          return (
+                            <ProjectCard
+                              key={ index }
+                              { ...project }
+                            />
+                          )
+                        })
+                      }
                     </Row>
-                </Container>
-                <img className="background-image-right" src={ colorSharp2 } alt="" />
-            </section >
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">
+                    <p>Em breve mais projetos!</p>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <p>Em breve mais projetos!</p>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </div>
+          </Col>
+        </Row>
+      </Container >
+      <img className="background-image-right" src={ colorSharp2 } alt="" />
+    </section >
 
-        </>
-    )
+  )
+
 }
 
 export default Projects
